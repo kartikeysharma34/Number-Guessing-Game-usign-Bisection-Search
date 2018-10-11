158c320211dbddefd8b5d8611b6e09475fa38867
Kartikey Sharma <36727029+kartikeysharma34@users.noreply.github.com>
2018-10-12 00:27:47 +0530

    Update numberguess.py

--- numberguess.py
+++ numberguess.py
@@ -1,6 +1,6 @@
 low = 0 
 high = 100 
-ans = (low+high)/2 
+ans = int((low+high)/2) 
 print ("Please think of a number between 0 and 100!")
 print ("Is your secret number " + str(ans) + "?")
 response = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ") 
@@ -8,14 +8,14 @@ response = str(response)
 while response != "c": 
    if response == "h":
         high = ans 
-        ans = (low + high)/2 
+        ans = int((low + high)/2) 
         print ("Is your secret number " + str(ans) + "?")
         response = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")  
         response = str(response) 
 
    if response == "l": 
         low = ans 
-        ans = (low + high)/2 
+        ans = int((low + high)/2) 
         print ("Is your secret number " + str(ans) + "?")
         response = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")  
         response = str(response)
